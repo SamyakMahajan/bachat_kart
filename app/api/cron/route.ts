b773b0de644b07a35cda18c54f0b34501e1460ad
SamyakMahajan <ketann682@gmail.com>
2024-01-06 17:16:05 +0530

    g

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,6 +6,11 @@ import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } f
 import { NextResponse } from "next/server";
 // import { Product } from "@/types";
 
+export const maxDuration=300;
+export const dynamic='force-dynamic';
+export const revalidate=0;
+
+
 
 export async function GET(){
     try {   
